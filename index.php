6f36807706f4637861e7a4f523af754c1a7d203a
ksturgeong <ksturgeongcc@gmail.com>
2024-10-22 11:03:13 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML and CSS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>This is the largest header</h1>
+    <h2>Second largest header</h2>
+    <h3>Thirsd largest header</h3>
+    <h4>Fourth largest header</h4>
+    <h5>Fifth largest header</h5>
+    <h6>Sixth lartest header</h6>
+    <p>This is a paragraph</p>
+
+    <!-- Adding an external link -->
+     <a href="https://google.com" target="_blank">Link to google</a>
+
+     <!-- Creating a table -->
+      <table>
+        <thead>
+          <tr>
+            <td>Column One</td>
+            <td>Column Two</td>
+            <td>Column Three</td>
+          </tr>  
+        </thead>
+        <tbody>
+        <tr>
+            <td>Column One data</td>
+            <td>Column Two data</td>
+            <td>Column Three data</td>
+          </tr>  
+        </tbody>
+      </table>
+
+
+</body>
+</html>
